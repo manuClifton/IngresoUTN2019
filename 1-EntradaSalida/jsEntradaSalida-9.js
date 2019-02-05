663db07d521f7cc4597c5b375dea245ddd2ed238
manuClifton <42977735+manuClifton@users.noreply.github.com>
2019-02-05 17:07:44 -0300

    TERMINADO
    Realizados los 10 ejercicios de entrada y salida de datos.

--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,14 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+    var num1;
+    var aumento;
+    var resultado;
 
-	
+    num1=parseInt(document.getElementById("sueldo").value);
+    aumento= num1*0.10;
+    resultado=num1+aumento;
+
+    document.getElementById("resultado").value=resultado;
+    
 }
