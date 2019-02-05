a22fa631396d97e484a00df83c9a34c370c53d10
manuClifton <42977735+manuClifton@users.noreply.github.com>
2019-02-05 13:11:38 -0300

    Hasta ejercicio 7
    Se realizo el ejercicio 7

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -2,23 +2,41 @@
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
+
+    var num1;
+    var num2;
+    var resultado;
+
 function sumar()
 {	
-	
+    num1=parseInt(document.getElementById("numeroUno").value);
+    num2=parseInt(document.getElementById("numeroDos").value);
+    resultado=num1+num2;
+    alert(resultado);
+
 }
 
 function restar()
 {
-	
+	num1=parseInt(document.getElementById("numeroUno").value);
+    num2=parseInt(document.getElementById("numeroDos").value);
+    resultado=num1-num2;
+    alert(resultado);
 }
 
 function multiplicar()
 { 
-	
+	num1=parseInt(document.getElementById("numeroUno").value);
+    num2=parseInt(document.getElementById("numeroDos").value);
+    resultado=num1*num2;
+    alert(resultado);
 }
 
 function dividir()
 {
-	
+	num1=parseInt(document.getElementById("numeroUno").value);
+    num2=parseInt(document.getElementById("numeroDos").value);
+    resultado=num1/num2;
+    alert(resultado);
 }
 
