b4bbbf931ed6c6d2e85a4f4597bbbb534c3f24ca
manuClifton <42977735+manuClifton@users.noreply.github.com>
2019-02-04 22:18:44 -0300

    Ejercicios
    Primer ejercicio

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -2,6 +2,14 @@
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function Mostrar()
 {	
-	
+	var nombre;
+    var edad;
+    var msj1= "Usted se llama ";
+    var msj2=" y tiene ";
+
+    nombre = document.getElementById("elNombre").value;
+    edad = document.getElementById("laEdad").value;
+    alert(msj1+nombre+msj2+edad);
+    
 }
 
--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,14 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
+var n1;
+var n2;
+var n3;
+n1=document.getElementById("numeroUno").value;
+
+n2=document.getElementById("numeroDos").value;
+
+alert("la suma es "+n1+n2);
 
 }
 
