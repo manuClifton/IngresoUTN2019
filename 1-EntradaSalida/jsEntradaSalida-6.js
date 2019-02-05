6a422de704ec5b182f8bc101500506744de31ad4
manuClifton <42977735+manuClifton@users.noreply.github.com>
2019-02-04 22:28:47 -0300

    Hasta ejercicio 6
    se realizo hasta el ejercicio 6

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,14 +3,17 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
-var n1;
-var n2;
-var n3;
-n1=document.getElementById("numeroUno").value;
+    var n1;
+    var n2;
+    var resultado;
 
-n2=document.getElementById("numeroDos").value;
+    n1=document.getElementById("numeroUno").value;
+    n1=parseInt(n1);
+    n2=document.getElementById("numeroDos").value;
+    n2=parseInt(n2);
+    resultado = n1+n2;
 
-alert("la suma es "+n1+n2);
+    alert("la suma es "+resultado);
 
 }
 
